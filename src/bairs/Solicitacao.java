03d8a9d15ecc6813170e406cfa592dc7f6721ec5
simpletextbr <simpletextbr@gmail.com>
2020-04-02 13:52:13 -0300

    feat: add ID into the Solicitacao

--- src/bairs/Solicitacao.java
+++ src/bairs/Solicitacao.java
@@ -2,13 +2,23 @@
 package bairs;
 
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.Random;
 
 
 public class Solicitacao {
+    private Random ID = new Random();
     private String Descricao;
     private List <String> Categoria;
-   
+    
+    public Random getID() {
+         ID.nextInt(100);
+        return ID;
+    }
+
+    public void setID(Random ID) {
+        this.ID = ID;
+    }
+    
     public String getDescricao() {
         return Descricao;
     }
@@ -27,12 +37,15 @@ public class Solicitacao {
 
     public void fazerSolicitacao(){
         Solicitacao pedido = new Solicitacao();
+        pedido.setID(ID);
         pedido.setDescricao(Descricao);
         pedido.setCategoria(Categoria);
     }
  
-    
-    
+    public void cancelarSolicitacao(){
+        
+    }
+
     
     
 }
