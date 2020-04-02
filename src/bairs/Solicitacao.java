175083da4cdb7a64090693526840d4bac9b85ca7
simpletextbr <simpletextbr@gmail.com>
2020-04-02 13:33:25 -0300

    feat: Create Set,Get and variables were encapsulated

---
+++ src/bairs/Solicitacao.java
@@ -0,0 +1,34 @@
+
+package bairs;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+
+public class Solicitacao {
+    private String Descricao;
+    private List <String> Categoria;
+   
+    public String getDescricao() {
+        return Descricao;
+    }
+
+       public void setDescricao(String Descricao) {
+        this.Descricao = Descricao;
+    }
+
+    public List <String> getCategoria() {
+        return Categoria;
+    }
+
+    public void setCategoria(List <String> Categoria) {
+        this.Categoria = Categoria;
+    }
+
+
+ 
+    
+    
+    
+    
+}
