530c7c648ad731b709346bcbaa579ff2c8d9710e
Bruno & Lud <Bruno & Lud@DESKTOP-KRKO5OR>
2020-04-08 20:10:42 -0300

    feat: Encapsula campos

--- src/bairs/Anuncio.java
+++ src/bairs/Anuncio.java
@@ -11,9 +11,65 @@ package bairs;
  */
 public class Anuncio {
     
-     String name;
-     String description;
-     Double price;
-     String category;
+     private String name;
+     private String description;
+     private Double price;
+     private String category;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the price
+     */
+    public Double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
     
 }
