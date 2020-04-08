745f9780891d488b0ec40355d1331fe906a6f5db
Thiago <>
2020-04-08 20:56:29 -0300

    feat: cria classe usuario

---
+++ src/bairs/modelo/Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bairs.modelo;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author CLIENTE
+ */
+public class Usuario {
+    private String nomeCompleto;
+    private Double CPF;
+    private Calendar dataNascimento;
+    private String email;
+    private String usuario;
+    private String senha;
+    private Double codigoVerificacao;
+}
