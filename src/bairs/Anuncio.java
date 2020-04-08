ac71b90b69573693bd5109b32839190f1c6bf12d
Bruno & Lud <Bruno & Lud@DESKTOP-KRKO5OR>
2020-04-08 20:07:04 -0300

    feat: Cria classe Anuncio

---
+++ src/bairs/Anuncio.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bairs;
+
+/**
+ *
+ * @author Bruno & Lud
+ */
+public class Anuncio {
+    
+     String name;
+     String description;
+     Double price;
+     String category;
+    
+}
