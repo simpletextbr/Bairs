1df97b2b48e558a6e25ef6f42f23327dadc2c561
Thiago <CLIENTE@CLIENTE-PC>
2020-04-08 21:27:07 -0300

    feat: cria metodos cadastrar e entrar

--- src/bairs/modelo/Usuario.java
+++ src/bairs/modelo/Usuario.java
@@ -19,4 +19,35 @@ public class Usuario {
     private String usuario;
     private String senha;
     private Double codigoVerificacao;
+
+    public void cadastrarUsuario(String nomeCompleto, Double CPF, Calendar dataNascimento, String email, String usuario, String senha) {
+        this.nomeCompleto = nomeCompleto;
+        this.CPF = CPF;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+    public String entrar(String usuario, String senha)
+  {
+        if(this.usuario.equals(usuario) && this.senha.equals(senha))
+            return "Sucesso";
+        else
+            return "Usuario ou senha incorretos";
+    }
+     public String entrarFacebook(String email, String senha){
+        if(this.email.equals(email) && this.senha.equals(senha))
+            return "Sucesso";
+        else
+            return "Usuario ou senha incorretos";
+    }
+     public String entrarGoogle(String email, String senha){
+        if(this.email.equals(email) && this.senha.equals(senha))
+            return "Sucesso";
+        else
+            return "Usuario ou senha incorretos";
+    }
 }
+
+ 
+
