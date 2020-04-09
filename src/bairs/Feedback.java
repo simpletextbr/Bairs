8b8c2cd78101f23d27eb1283ace5b544222f383d
TECAF <TECAF@TECAF>
2020-04-08 22:22:43 -0300

    Feat 1: Melhoria do metodo setAvaliacao, inserido trava para o usuário não digitar 		valores menores que 0 ou maiores que 5. Feat 2: Melhoria do metodo setComentario, inserido trava para o usuário não 		digitar valores menores que 2 ou maiores que 500. Feat 3: Alteração do metodo construtor da classe Feedback (Não estava usando os 			metodos get e set).

--- src/bairs/Feedback.java
+++ src/bairs/Feedback.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bairs.modelo;
+package bairs;
 
 import java.util.Objects;
 import java.util.Random;
