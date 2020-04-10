fa861604c72c1ec0e777867ea0d92c4928c6f8e9
Luana Emanuela <luanaemanuela02@gmail.com>
2020-04-09 23:58:26 -0300

    feat: Cria classe Impulsionamento

---
+++ src/bairs/Impulsionamento.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bairs;
+
+/**
+ *
+ * @author luana
+ */
+public class Impulsionamento {    
+    String nomenclatura;
+    String duracao;
+    String desconto;
+    
+}
