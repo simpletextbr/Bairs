8c86fbf7f67ed154ce56ac39499dd4e9d9e85792
Luana Emanuela <luanaemanuela02@gmail.com>
2020-05-28 13:45:46 -0300

    feat: Testa classe Impulsionamento
    Obs: Método Impulsionamento Básico

---
+++ test/bairs/ImpulsionamentoTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bairs;
+
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author luana
+ */
+public class ImpulsionamentoTest {
+    
+    public ImpulsionamentoTest() {       
+    }
+    
+    @Test
+    public void Impulsinamento() {
+        Impulsionamento impul = new Impulsionamento();
+        System.out.println(impul.basico());
+            
+}
+    
+    
+   
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
