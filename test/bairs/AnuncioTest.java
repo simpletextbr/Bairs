c068e4aaebb2c943a9939107664d8cd0b600b10c
Gabriel Bryan <gbryan6@hotmail.com>
2020-05-28 23:59:01 -0300

    feat: Cria teste Anuncio

---
+++ test/bairs/AnuncioTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bairs;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class AnuncioTest {
+    
+    public AnuncioTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void Anuncio(){
+     String name = "Computador";
+     String description = "Computador bem velho para retirada de peças";
+     Double price = 150.00;
+     String category = "Informatica";
+    }
+}
